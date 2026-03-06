234c70e05824a38b22db3321a5b50a413142f62c
pedro gonzaga <pedro.h.castro21@aluno.senai.br>
2026-03-06 11:33:20 -0300

    Controle  do menu mobile

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// 1ª Digitação (JS Completo)
+// Controle do  menu mobile
+const menuIcon = document.querySelector('#menu-icon');
+const navLit = document.querySelector('.navlist');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navLit.classList.toggle('open');
+
+    // bloquear scroll quando menu aberto
+    document.body.style.overflow = navlist.classList.contains('open') ? 'hidden' : 'auto';
+});
+
+// fechar menu ao clicar em links
+document.querySelectorAll('.navlist a').forEach(link => {
+    link.addEventListener('click', () => {
+        menuIcon.classList.remove('bx-x');
+        navLit.classList.remove('open');
+        document.body.style.overflow = 'auto';
+    });
+});
+
+// fechar menu ao rolar 
+window.addEventListener('scroll', () => {
+    if(navlist.classList.contains('open')) {
+        menuIcon.classList.remove('bx-x');
+        navlist.classList.remove('open');
+        document.body.style.overflow = 'auto';
+    }
+});
